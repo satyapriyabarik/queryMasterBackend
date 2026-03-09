6fa82066a3497d942f48a9f472d13da3e6346ab4
Satyapriya Barik <satya.barik27@gmail.com>
2026-03-09 23:09:16 +0530

    added cors

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ require("dotenv").config();
 const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./src/schema/schema");
 const { resolvers } = require("./src/resolvers");
-const server = new ApolloServer({ typeDefs, resolvers });
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: {
+    origin: ["http://localhost:3000"], // allow React app
+    credentials: true
+  }
+});
 
 server.listen({ port: 4000 }).then(({ url }) => {
   console.log(`🚀 GraphQL Server ready at ${url}`);
