8a54ed7c7e6692e7074cdaee920aa848e656be38
Satyapriya Barik <satya.barik27@gmail.com>
2026-03-13 20:30:11 +0530

    chages in access

--- index.js
+++ index.js
@@ -16,39 +16,18 @@
 //   console.log(`🚀 GraphQL Server ready at ${url}`);
 // });
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const { ApolloServer } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server");
 const { typeDefs } = require("./src/schema/schema");
 const { resolvers } = require("./src/resolvers");
 
-async function startServer() {
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  cors: {
+    origin: "*"
+  }
+});
 
-  const app = express();
-
-  app.use(cors({
-    origin: [
-      "http://localhost:3000",
-      "http://querybuilder-frontend.s3-website.ap-south-1.amazonaws.com"
-    ],
-    credentials: true
-  }));
-
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers
-  });
-
-  await server.start();
-
-  server.applyMiddleware({
-    app,
-    path: "/graphql"
-  });
-
-  app.listen(4000, () => {
-    console.log("🚀 Server running on http://localhost:4000/graphql");
-  });
-}
-
-startServer();
+server.listen({ port: 4000 }).then(({ url }) => {
+  console.log(`🚀 GraphQL Server ready at ${url}`);
+});
