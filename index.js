61f832905ba17944116fc79ea4d973400af4844b
Satyapriya Barik <satya.barik27@gmail.com>
2026-03-13 21:26:17 +0530

    resolved cors issue trial

--- index.js
+++ index.js
@@ -16,18 +16,41 @@
 //   console.log(`🚀 GraphQL Server ready at ${url}`);
 // });
 require("dotenv").config();
-const { ApolloServer } = require("apollo-server");
+const express = require("express");
+const cors = require("cors");
+const { ApolloServer } = require("apollo-server-express");
+
 const { typeDefs } = require("./src/schema/schema");
 const { resolvers } = require("./src/resolvers");
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  cors: {
-    origin: "*"
-  }
-});
+async function startServer() {
 
-server.listen({ port: 4000 }).then(({ url }) => {
-  console.log(`🚀 GraphQL Server ready at ${url}`);
-});
+  const app = express();
+
+  app.use(cors({
+    origin: [
+      "http://localhost:3000",
+      "http://querybuilder-frontend.s3-website.ap-south-1.amazonaws.com"
+    ],
+    credentials: true
+  }));
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  await server.start();
+
+  server.applyMiddleware({
+    app,
+    path: "/graphql",
+    cors: false
+  });
+
+  app.listen(4000, () => {
+    console.log("🚀 GraphQL Server ready at http://localhost:4000/graphql");
+  });
+}
+
+startServer();
