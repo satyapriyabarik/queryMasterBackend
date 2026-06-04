d3afce658e572345b6d73d1b1cb8e0fcd3c10a3a
Satyapriya Barik <satya.barik27@gmail.com>
2026-06-04 16:41:42 +0530

    whitelisting localhost

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-// require("dotenv").config();
-// const { ApolloServer } = require("apollo-server");
-// const { typeDefs } = require("./src/schema/schema");
-// const { resolvers } = require("./src/resolvers");
-
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   cors: {
-//     origin: ["http://localhost:3000", "http://querybuilder-frontend.s3-website.ap-south-1.amazonaws.com"], // allow React app
-//     credentials: true
-//   }
-// });
-
-// server.listen({ port: 4000 }).then(({ url }) => {
-//   console.log(`🚀 GraphQL Server ready at ${url}`);
-// });
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
@@ -22,7 +5,6 @@ const bodyParser = require("body-parser");
 
 const { typeDefs } = require("./src/schema/schema");
 const { resolvers } = require("./src/resolvers");
-
 async function startServer() {
 
   const app = express();
@@ -42,7 +24,7 @@ async function startServer() {
   server.applyMiddleware({
     app,
     path: "/graphql",
-    cors: false   // IMPORTANT: disable Apollo CORS
+    cors: { origin: 'http://localhost:3000', credentials: false }   // IMPORTANT: disable Apollo CORS
   });
 
   const PORT = process.env.PORT || 4000;
